32583b9c7a785d9064be59aa5b8463dbb8675411
sofiegraham <sofiegraham@hotmail.com>
2017-11-05 17:57:23 -0800

    Setup app with webpack, express and react

---
+++ src/main.js
@@ -0,0 +1,10 @@
+console.log('File loaded yup yup!');
+import 'bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter';
+
+ 
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(<Counter/>, document.getElementById('mount'));
+});
